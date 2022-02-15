c2a1854d8ba71f5c699c22d6a7d4bfd49ef68fe6
speria846 <niwenyesigasperia@gmail.com>
2022-02-15 22:17:41 +0300

    first commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,15 @@
-
 fun main(){
-
-    var name ="john"
-    println(name)
+    var Name = "niwenyesiga speria"
+    println(Name)
+    var Age = "21"
+    println(Age)
+    var Phonenumber = "0787334058"
+    println(Phonenumber)
+    var Weight = "60"
+    println(Weight)
+    val iskenyan:Boolean =true
+    println(iskenyan) //output true
+    val isnotkenyan:Boolean =false
+    println(isnotkenyan) //output false
 
 }
