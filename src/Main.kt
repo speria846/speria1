db85c1eabd2795fb2859d974e9386b525646cd40
speria846 <niwenyesigasperia@gmail.com>
2022-02-20 19:52:07 +0300

    first commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,15 +1,28 @@
-fun main(){
-    var Name = "niwenyesiga speria"
-    println(Name)
-    var Age = "21"
-    println(Age)
-    var Phonenumber = "0787334058"
-    println(Phonenumber)
-    var Weight = "60"
-    println(Weight)
-    val iskenyan:Boolean =true
-    println(iskenyan)
-    val isnotkenyan:Boolean =false
-    println(isnotkenyan)
 
+fun main(){
+    modulus(27,4)
+    add(23,45,21,34)
+    greetings()
+    intresting()
+}
+
+fun modulus(a: Int, b: Int): Int{
+    var modulus = a%b
+    println(modulus)
+    return modulus
+
+}
+fun add(s: Int, p: Int, e: Int,r: Int){
+    var sum = s+p+e+r
+    println(sum)
+
+}
+fun greetings(){
+    var name = "speria"
+    println("hello "+name)
+
+}
+fun intresting(){
+    var intresting = "i like loughing"
+    println(intresting)
 }
